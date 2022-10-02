11fe2113f51cbd759abb046135a322a8046e1788
Kli Kli <60774993+klikli-dev@users.noreply.github.com>
2022-10-02 10:53:18 +0200

    Remove hardcoded assignee

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function run() {
       return;
     }
 
-    const created = await lib.createNewIssue(token, owner, repoName, issue.data.title, contentOfNewIssue, ['soichisumi'], [], issue.data.html_url);
+    const created = await lib.createNewIssue(token, owner, repoName, issue.data.title, contentOfNewIssue, [], [], issue.data.html_url);
 
     core.setOutput('created', created);
   } 
